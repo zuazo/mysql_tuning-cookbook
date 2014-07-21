1ef436689a6086f75909c12a7b2c84069a4befd3
Xabier de Zuazo <xabier@onddo.com>
2014-07-21 10:09:20 +0200

    version bumped to 0.2.0 WiP

--- metadata.rb
+++ metadata.rb
@@ -8,7 +8,7 @@ description <<-EOS
 Tries to create MySQL configuraiton better suited for your system.
 EOS
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.0'
+version '0.2.0' # WiP
 
 supports 'amazon'
 supports 'centos'
