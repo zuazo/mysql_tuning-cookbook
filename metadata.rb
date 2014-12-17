10940af4d5dfce6d07a26a7856edfdf1af8aa96a
Xabier de Zuazo <xabier@onddo.com>
2014-12-17 13:23:46 +0100

    metadata: mysql-chef_gem < 2

--- metadata.rb
+++ metadata.rb
@@ -38,7 +38,7 @@ supports 'ubuntu'
 
 depends 'mysql', '>= 5.0.0'
 depends 'ohai'
-depends 'mysql-chef_gem', '~> 1.0'
+depends 'mysql-chef_gem', '< 2'
 
 recipe 'mysql_tuning::default', 'Creates MySQL configuration files.'
 recipe 'mysql_tuning::ohai_plugin', 'Enables MySQL ohai plugin.'
