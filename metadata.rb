d350a671fcb36e4d2e9ab36dd7f188b49a511c01
Xabier de Zuazo <xabier@onddo.com>
2014-12-18 14:50:08 +0100

    version bumped to 0.4.0 WiP

--- metadata.rb
+++ metadata.rb
@@ -26,7 +26,7 @@ description <<-EOS
 Tries to create MySQL configuraiton better suited for your system.
 EOS
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.3.0'
+version '0.4.0' # WiP
 
 supports 'amazon'
 supports 'centos'
@@ -38,7 +38,7 @@ supports 'ubuntu'
 
 depends 'mysql', '>= 5.0.0'
 depends 'ohai', '~> 2.0'
-depends 'mysql-chef_gem', '< 2'
+depends 'mysql-chef_gem', '< 2.0'
 
 recipe 'mysql_tuning::default', 'Creates MySQL configuration files.'
 recipe 'mysql_tuning::ohai_plugin', 'Enables MySQL ohai plugin.'
